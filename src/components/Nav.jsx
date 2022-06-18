5259c577609c4eb5bd6203f8dba6e059585890c0
kimimberlyyen <work223305@gmail.com>
2022-06-18 09:03:14 +0800

    feat: 🎸 first commit
    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <>
+      <div className="w-24 h-24">
+        <img
+          className="w-full h-full"
+          src="src/img/Troll-Face.png"
+          alt="home_icon"
+        />
+      </div>
+      <h1 className=" text-3xl font-bold  un"> Meme Generator</h1>
+    </>
+  )
+}
